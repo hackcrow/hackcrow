8f93648c428198c52ca5c6dd48118a5b6cb0d47e
Rafael Andrade <farar12@gmail.com>
2026-05-15 13:29:58 -0700

    Update library.js

--- workout/js/library.js
+++ workout/js/library.js
@@ -69,7 +69,9 @@ function renderEjercicios(filtro = "todos") {
   grid.innerHTML = lista.map((e) => `
     <div class="exercise-card">
       <button class="edit-btn" data-id="${e.id}" title="Editar">
-        ✎
+        <svg viewBox="0 0 24 24" class="edit-icon" aria-hidden="true">
+          <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zm17.71-10.04a1.003 1.003 0 0 0 0-1.42l-2.5-2.5a1.003 1.003 0 0 0-1.42 0l-1.96 1.96 3.75 3.75 2.13-1.79z"/>
+        </svg>
       </button>
 
       <div class="ex-muscle">${e.musculo}</div>
