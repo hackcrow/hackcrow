aade6fb6abcb91793e7807229e47abc50fce0f69
Rafael Andrade <farar12@gmail.com>
2026-05-19 09:14:49 -0700

    Update routines.js

--- workout/js/routines.js
+++ workout/js/routines.js
@@ -1,5 +1,6 @@
-const ROUTINE_SUPABASE_URL = "https://xqcqzvcvqpwbjdsdxcan.supabase.co";
-const ROUTINE_SUPABASE_KEY = "TU_SUPABASE_KEY";
+const SUPABASE_URL = "https://xqcqzvcvqpwbjdsdxcan.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxY3F6dmN2cXB3Ympkc2R4Y2FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NjI3OTQsImV4cCI6MjA5NDQzODc5NH0.vAwo9NS7MoiVCFikfk39YM9nBr2usyB4jMW2uYXhH98";
+
 
 const routineClient = window.supabase.createClient(
   ROUTINE_SUPABASE_URL,
