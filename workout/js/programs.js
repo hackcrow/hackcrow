9f853d1384e09e76daef26e4cd13547bbc21707c
Rafael Andrade <farar12@gmail.com>
2026-06-01 09:59:02 -0700

    Update programs.js

--- workout/js/programs.js
+++ workout/js/programs.js
@@ -1,1 +1,37 @@
 
+async function cargarProgramas() {
+  console.log("cargar programas");
+}//cargarProgramas()
+
+function abrirProgramLightbox(){
+
+  document
+    .getElementById(
+      "programLightbox"
+    )
+    .classList.add(
+      "open"
+    );
+
+}//abrirProgramLightbox()
+
+function cerrarProgramLightbox(){
+
+  document
+    .getElementById(
+      "programLightbox"
+    )
+    .classList.remove(
+      "open"
+    );
+
+}//cerrarProgramLightbox()
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+
+    cargarProgramas();
+
+  }
+);//DOMContentLoaded
