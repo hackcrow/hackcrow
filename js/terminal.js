a7d1f29974778ec9fa65872728ef509abc4f7e78
Rafael Andrade <farar12@gmail.com>
2026-07-08 08:06:12 -0700

    Update terminal.js

--- js/terminal.js
+++ js/terminal.js
@@ -1,1 +1,14 @@
+function initializeTerminal() {
 
+    const output = document.getElementById("terminalOutput");
+
+    output.innerHTML = `
+        <div class="line">Connecting to Hackcrow Network...</div>
+        <div class="line">Authentication Successful</div>
+        <div class="line"></div>
+        <div class="line">Type <span class="highlight">HELP</span> to begin.</div>
+    `;
+
+    document.getElementById("commandInput").focus();
+
+}
