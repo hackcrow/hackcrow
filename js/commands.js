eda1e1863d6a79583aa861d8c8159bea81b012e4
Rafael Andrade <farar12@gmail.com>
2026-07-09 08:02:29 -0700

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -3,20 +3,69 @@ const commands = {
     help(){
 
         return [
+
+            "═══════════════════════════════════════════════",
+            "              HACKCROW COMMAND INDEX",
+            "═══════════════════════════════════════════════",
             "",
-            "AVAILABLE COMMANDS",
-            "------------------",
+
+            "[ SYSTEM ]",
+
             "help        Show available commands",
-            "about       About Hackcrow",
+            "about       About Hackcrow OS",
+            "version     Show current version",
+            "clear       Clear terminal",
+            "",
+
+            "[ APPLICATIONS ]",
+
             "gym         Open HackGym",
             "translate   Open Aurebesh Translator",
             "words       Open 5 Daily Words",
-            "tools       Open tools",
-            "clear       Clear terminal",
+            "tools       Open Tools",
+            "",
+
+            "Type a command and press ENTER.",
             ""
+
         ];
+
+    },
+
+    about(){
+
+        return [
+
+            "Hackcrow OS",
+            "Version 3.0",
+            "",
+            "A hacker-inspired personal workspace",
+            "created by Rafael.",
+            ""
+
+        ];
+
+    },
+
+    version(){
+
+        return [
+
+            "Hackcrow OS v3.0",
+            ""
+
+        ];
+
+    },
+
+    clear(){
+
+        terminalOutput.innerHTML = "";
+
+        createPrompt();
+
+        return null;
 
     }
 
 };
-
