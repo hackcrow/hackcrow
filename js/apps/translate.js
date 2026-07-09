4b59a2e527f8446cd372f541003cf126061ad896
Rafael Andrade <farar12@gmail.com>
2026-07-09 14:18:03 -0700

    Update translate.js

--- js/apps/translate.js
+++ js/apps/translate.js
@@ -1,1 +1,14 @@
+function openTranslator(){
 
+    printLine("");
+    printLine("Launching Aurebesh Translator...");
+    printLine("Loading Galactic Dictionary...");
+    printLine("Initializing translator...");
+
+    setTimeout(() => {
+
+        window.location.href = "aurebesh/index.html";
+
+    }, 1200);
+
+}
