507ecba3accba996c170333c6a2ed40c21dad48d
Rafael Andrade <farar12@gmail.com>
2026-07-09 07:57:06 -0700

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,1 +1,22 @@
+const commands = {
+
+    help(){
+
+        return [
+            "",
+            "AVAILABLE COMMANDS",
+            "------------------",
+            "help        Show available commands",
+            "about       About Hackcrow",
+            "gym         Open HackGym",
+            "translate   Open Aurebesh Translator",
+            "words       Open 5 Daily Words",
+            "tools       Open tools",
+            "clear       Clear terminal",
+            ""
+        ];
+
+    }
+
+};
 
