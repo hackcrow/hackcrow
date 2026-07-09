5fdc62e4fc072589b67b10332762ad339cad7f56
Rafael Andrade <farar12@gmail.com>
2026-07-09 14:19:40 -0700

    Update tools.js

--- js/apps/tools.js
+++ js/apps/tools.js
@@ -1,1 +1,13 @@
+function openTools(){
 
+    printLine("");
+    printLine("Launching Tools...");
+    printLine("Loading utilities...");
+
+    setTimeout(() => {
+
+        window.location.href = "tools/index.html";
+
+    }, 1200);
+
+}
