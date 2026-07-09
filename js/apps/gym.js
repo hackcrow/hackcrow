5a6dfbbd3a9b948cfa034fd6885ae83397a878e7
Rafael Andrade <farar12@gmail.com>
2026-07-09 14:17:41 -0700

    Update gym.js

--- js/apps/gym.js
+++ js/apps/gym.js
@@ -1,1 +1,14 @@
+function openHackGym(){
 
+    printLine("");
+    printLine("Launching HackGym...");
+    printLine("Loading workout database...");
+    printLine("Initializing modules...");
+
+    setTimeout(() => {
+
+        window.location.href = "workout/index.html";
+
+    }, 1200);
+
+}
