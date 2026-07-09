2c8218b7c55f53c04119aced48d5e76cf8abfc5c
Rafael Andrade <farar12@gmail.com>
2026-07-09 14:18:21 -0700

    Update words.js

--- js/apps/words.js
+++ js/apps/words.js
@@ -1,1 +1,14 @@
+function openWords(){
 
+    printLine("");
+    printLine("Launching 5 Daily Words...");
+    printLine("Loading dictionary...");
+    printLine("Synchronizing database...");
+
+    setTimeout(() => {
+
+        window.location.href = "words/index.html";
+
+    }, 1200);
+
+}
