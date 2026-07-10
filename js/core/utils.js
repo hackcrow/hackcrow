6a4e3ce8c1ee46eb8f35a4ddf7f740ed0ab38151
Rafael Andrade <farar12@gmail.com>
2026-07-10 09:01:39 -0700

    Update utils.js

--- js/core/utils.js
+++ js/core/utils.js
@@ -1,1 +1,38 @@
+function launchApplication(name, messages, url){
 
+    terminalOutput.innerHTML = "";
+
+    const lines = [
+
+        `Launching ${name}...`,
+        ...messages,
+        "",
+        "Done.",
+        "",
+        "Opening application..."
+
+    ];
+
+    let index = 0;
+
+    const interval = setInterval(() => {
+
+        printLine(lines[index]);
+
+        index++;
+
+        if(index >= lines.length){
+
+            clearInterval(interval);
+
+            setTimeout(() => {
+
+                window.location.href = url;
+
+            }, 500);
+
+        }
+
+    }, 250);
+
+}
